7f6d89bbd7d4c276f6e961247fcec33fa005c92a
Jas Aggarwal <1698584+rottitime@users.noreply.github.com>
2022-12-12 17:02:35 +0000

    data test page

---
+++ src/pages/test/api.tsx
@@ -0,0 +1,29 @@
+import { useQuery, useQueryClient } from '@tanstack/react-query'
+import React from 'react'
+
+function ApiPage() {
+  const { isLoading, isSuccess, data } = useQuery({
+    queryKey: ['todos'],
+    queryFn: fetchTestData
+  })
+
+  console.log({ data })
+
+  if (isLoading) return <p>Loading...</p>
+  if (isSuccess)
+    return (
+      <div>
+        <p>api data:</p>
+        <textarea style={{ width: '80%', height: '200px' }}>
+          {JSON.stringify({ data })}
+        </textarea>
+      </div>
+    )
+}
+
+export default ApiPage
+
+export const fetchTestData = async (): Promise<string[]> => {
+  const res = await fetch('https://swapi.dev/api/planets/1/')
+  if (res.ok) return res.json()
+}
