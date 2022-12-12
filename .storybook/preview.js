9a57cf1644fec14a836589c9c9f405af48b80028
Jas Aggarwal <1698584+rottitime@users.noreply.github.com>
2022-12-12 12:18:56 +0000

    storybook config

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,33 @@
+import ThemeProvider from '../src/components/ThemeProvider/ThemeProvider'
+import LocalizationProvider from '../src/components/LocalizationProvider/LocalizationProvider'
+import { setConfig } from 'next/config'
+import { publicRuntimeConfig } from '../next.config'
+setConfig({ publicRuntimeConfig })
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
-      date: /Date$/,
-    },
+      date: /Date$/
+    }
   },
+  backgrounds: {
+    default: 'White',
+    values: [
+      { name: 'Ellandi dark grey', value: '#AEAEAE' },
+      { name: 'Ellandi light', value: '#E9EAEC' },
+      { name: 'White', value: '#fff' }
+    ]
+  }
 }
+
+export const decorators = [
+  (Story) => (
+    <ThemeProvider>
+      <LocalizationProvider>
+        <Story />
+      </LocalizationProvider>
+    </ThemeProvider>
+  )
+]
