c0f45730ac9684dea961b82cadc8acda8628ccfc
Jas Aggarwal <1698584+rottitime@users.noreply.github.com>
2022-12-13 12:48:46 +0000

    layout

--- src/components/Layout/PublicLayout.tsx
+++ src/components/Layout/PublicLayout.tsx
@@ -1,99 +1,90 @@
 import { Box, styled, Typography } from '@mui/material'
 import { Template } from '@/components'
-import { TimeFast, ConnectionRelation } from '@/components/Icon'
+import { TimeFast, ConnectionRelation, Logo } from '@/components/Icon'
 import { ComponentProps } from 'react'
 
 type Props = ComponentProps<typeof Box>
 
+const borderRadios = 40
+const overlap = 50
+
 const Columns = styled(Box)`
   display: flex;
-  min-height: 100vh;
+  /* min-height: 100vh; */
+
+  min-height: auto;
+  box-shadow: 0px 4px 4px rgb(0 0 0 / 25%);
+  backdrop-filter: blur(68px);
+  border-radius: ${borderRadios}px;
+  overflow: hidden;
+  background: linear-gradient(204.28deg, #e6cece 14.76%, #d1ccf4 97.11%);
+  /* max-width: 1050px; */
 
   .promo,
-  .content {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    position: relative;
+  .main-content {
     padding: ${(p) => p.theme.spacing(4)};
+    width: 100%;
   }
 
   .promo {
+    max-width: 342px;
     color: ${(p) => p.theme.colors.white};
+    padding-right: calc(${(p) => p.theme.spacing(4)} + ${overlap}px);
+    text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
     svg {
       font-size: 210px;
     }
   }
-  .title {
-    display: flex;
-    align-items: center;
-    margin-bottom: ${(p) => p.theme.spacing(4)};
-    gap: ${(p) => p.theme.spacing(4)};
-    position: absolute;
-    top: ${(p) => p.theme.spacing(4)};
-    left: ${(p) => p.theme.spacing(4)};
-    .logo {
-      color: ${(p) => p.theme.colors.white};
-      font-size: 45px;
-      gap: 10px;
-    }
-  }
-  .content {
+
+  .main-content {
     background-color: ${(p) => p.theme.colors.grey1};
     flex-grow: 1;
+    border-radius: ${borderRadios}px 0 0 ${borderRadios}px;
+    margin-left: -${overlap}px;
+    max-width: 915px;
+    box-shadow: 0px 4px 24px rgba(0, 0, 0, 0.25);
+  }
 
-    border-radius: 5px 0 0 5px;
-
-    &:after {
-      content: 'normal';
-      position: absolute;
-      inset: 0px -100vw 0px 0px;
-      z-index: -1;
-      background-color: inherit;
-      border-radius: inherit;
-      overflow: hidden;
-      box-shadow: -10px 0px 10px 1px rgba(0, 0, 0, 0.2);
-    }
+  .logo {
+    position: absolute;
+    bottom: 0px;
+    right: 20px;
+    font-size: 120px;
   }
 `
 
 const PublicLayout = ({ children, ...props }: Props) => (
   <Template>
+    <Logo className="logo" />
     <style jsx global>
       {`
         body {
-          // background: linear-gradient(
-          //     243.18deg,
-          //     #fdc5c5 25.45%,
-          //     rgba(242, 152, 222, 0) 100%
-          //   ),
-          //   linear-gradient(243.43deg, #ffa5a5 32.96%, rgba(255, 255, 255, 0) 82.03%),
-          //   #f39494;
-          background-color: #769cc1;
+          background: linear-gradient(
+            144.23deg,
+            rgba(209, 204, 244, 0.69) -8.57%,
+            #e6cece 94.27%
+          );
         }
       `}
     </style>
     <Columns {...props}>
       <div className="promo">
         <div className="title">
-          <TimeFast className="logo" />
-          <Typography variant="h1" component="h1">
+          {/* <img src="img/logo.png" className="logo" /> */}
+          {/* <TimeFast className="logo" /> */}
+          {/* <Typography variant="h1" component="h1">
             Rapid API
-          </Typography>
+          </Typography> */}
         </div>
 
-        <div className="middle-content">
-          <Typography sx={{ mb: 3 }}>
-            Join the platform to get more exciting features
-          </Typography>
-          <Box sx={{ textAlign: 'center' }}>
-            <ConnectionRelation />
-          </Box>
-        </div>
+        <Typography sx={{ mb: 3 }}>
+          Join the platform to get more exciting features
+        </Typography>
+        <Box sx={{ textAlign: 'center' }}>
+          <ConnectionRelation />
+        </Box>
       </div>
-      <Box className="content">
-        <div className="middle-content">{children}</div>
-      </Box>
+      <Box className="main-content">{children}</Box>
     </Columns>
   </Template>
 )
