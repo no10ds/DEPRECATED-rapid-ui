a0b7ba4b627a5deeac204005254f56bcb2f0786a
Toby Drane <tdrane@no10.gov.uk>
2023-01-24 18:48:24 +0000

    changelog pattern matching

--- generate_latest_changelog.js
+++ generate_latest_changelog.js
@@ -3,11 +3,20 @@ const fs = require('fs')
 const file = fs.readFileSync('./CHANGELOG.md', { encoding: 'utf-8' })
 
 const parsedLines = []
+let adding = false
+
 file.split(/\r?\n/).forEach((line) => {
-  parsedLines.push(line)
+  const pattern = /## v[0-9]\.[0-9]\.[0-9] - _[0-9]{4}-[0-9]{2}-[0-9]{2}_/
+  if (pattern.test(line)) {
+    adding = !adding
+  }
+
+  if (adding) {
+    parsedLines.push(line)
+  }
 })
 
-if (parsedLines.length === 1 && parsedLines[0] === '') {
+if (!parsedLines.length) {
   throw new Error(
     'It looks like there is no release information in the changelog. Please check it.'
   )
