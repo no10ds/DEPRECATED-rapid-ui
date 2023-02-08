a5d5483e967767db27e4dabbc8dcf63e2e278518
Jas Aggarwal <1698584+rottitime@users.noreply.github.com>
2023-02-08 15:56:27 +0000

    initial timeout unit testing

---
+++ src/__tests__/app.test.tsx
@@ -0,0 +1,28 @@
+import { screen } from '@testing-library/react'
+import fetchMock from 'jest-fetch-mock'
+import { renderWithProviders } from '@/lib/test-utils'
+import AppPage from '@/pages/_app'
+
+jest.useFakeTimers()
+jest.spyOn(global, 'setTimeout')
+
+describe('Page: Debug page', () => {
+  afterEach(() => {
+    fetchMock.resetMocks()
+  })
+
+  it('timeout ', async () => {
+    renderWithProviders(
+      <AppPage Component={jest.fn()} pageProps={undefined} router={null} />
+    )
+    expect(setTimeout).toHaveBeenCalledTimes(1)
+    expect(setTimeout).toHaveBeenLastCalledWith(expect.any(Function), 300000)
+
+    jest.runOnlyPendingTimers()
+
+    expect(fetchMock).toBeCalledWith('/api/oauth2/logout', {
+      credentials: 'include',
+      method: 'GET'
+    })
+  })
+})
