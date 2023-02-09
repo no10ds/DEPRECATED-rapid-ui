73a036ec0a4f77974bf04038b37bb6e7ac701b82
Jas Aggarwal <1698584+rottitime@users.noreply.github.com>
2023-02-09 16:25:11 +0000

    test: on valid auth

--- src/__tests__/login.test.tsx
+++ src/__tests__/login.test.tsx
@@ -2,17 +2,42 @@ import { screen, waitFor } from '@testing-library/react'
 import fetchMock from 'jest-fetch-mock'
 import { renderWithProviders } from '@/lib/test-utils'
 import LoginPage from '@/pages/login'
-import { MethodsResponse } from '@/service/types'
+import { AuthResponse, GetLoginResponse, MethodsResponse } from '@/service/types'
+
+const mockAuth: AuthResponse = { detail: 'success' }
+const mockLogin: GetLoginResponse = { auth_url: 'http://my-apiurl/' }
+
+const replaceSpy = jest.fn()
+jest.mock('next/router', () => ({
+  ...jest.requireActual('next/router'),
+  useRouter: jest.fn(() => ({
+    locale: 'en',
+    replace: replaceSpy
+  }))
+}))
 
 describe('Page: Login page', () => {
   afterEach(() => {
     fetchMock.resetMocks()
+    jest.clearAllMocks()
   })
 
-  it('renders', () => {
+  it('loading state', () => {
     renderWithProviders(<LoginPage />)
-
     expect(screen.getByRole('progressbar')).toBeVisible()
+  })
+
+  it('on valid auth redirects', async () => {
+    fetchMock.mockResponses(
+      [JSON.stringify(mockAuth), { status: 200 }],
+      [JSON.stringify(mockLogin), { status: 200 }]
+    )
+    renderWithProviders(<LoginPage />)
+    await waitFor(async () => {
+      expect(replaceSpy).toHaveBeenCalledWith({
+        pathname: '/'
+      })
+    })
   })
 
   // describe('permissions', () => {
