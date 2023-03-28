a85122e750ecddda0aaa3d300ad6f87b45794e70
Ricky Parkinson <2026055+rickyp72@users.noreply.github.com>
2023-03-28 12:38:46 +0100

    Set ui-user cred vars

--- ui-test/ui-test.spec.ts
+++ ui-test/ui-test.spec.ts
@@ -1,14 +1,27 @@
 import { test, expect } from '@playwright/test';
+const rapidUiUser = process.env.RAPID_UI_USERNAME;
+const rapidUiUserPw = process.env.RAPID_UI_PASSWORD;
 
 test('test', async ({ page }) => {
-
-//   Go to https://dev-test.getrapid.link/
   await page.goto('https://preprod.getrapid.link/');
-
-  // Go to https://dev-test.getrapid.link/login
   await page.goto('https://preprod.getrapid.link/login');
-
-  // Click text=Login
-  await page.locator('text=Login').click();
+  await page.getByRole('link', { name: 'Login' }).click();
+  await page.getByRole('textbox', { name: 'Username' }).click();
+  await page.getByRole('textbox', { name: 'Username' }).click();
+  await page.getByRole('textbox', { name: 'Username' }).fill(`${rapidUiUser}`);
+  await page.getByRole('textbox', { name: 'Password' }).click();
+  await page.getByRole('textbox', { name: 'Password' }).click();
+  await page.getByRole('textbox', { name: 'Password' }).fill(`${rapidUiUserPw}`);
+  await page.getByRole('button', { name: 'submit' }).click();
+  await page.getByRole('button', { name: 'Create User' }).click();
+  await page.getByRole('button', { name: 'Modify User' }).click();
+  await page.getByRole('button', { name: 'Download data' }).click();
+  await page.getByRole('button', { name: 'Upload data' }).click();
+  await page.getByRole('button', { name: 'Create Schema' }).click();
+  await page.getByRole('button', { name: 'Task Status' }).click();
+  await page.getByRole('link', { name: 'Home' }).click();
+  await page.getByRole('link', { name: 'Create User' }).nth(1).click();
+  await page.getByRole('link', { name: 'Home' }).click();
+  await page.getByRole('button', { name: 'account of current user' }).click();
+  await page.getByText('Logout').click();
 });
-// https://test.getrapid.link
